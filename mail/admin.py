b2532b7a3defeaab11ab50fa940d911b746ec09f
Artemova Dina <artemova@perx.ru>
2025-08-30 21:15:54 +0300

    add admin settings

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Message, Newsletter, MailingTry
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "full_name")
+    search_fields = ("email", "full_name")
+    list_filter = ("owner",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "topic")
+    list_filter = ("owner",)
+    search_fields = ("topic",)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "message", "owner")
+    list_filter = ("owner", "status")
+
+
+@admin.register(MailingTry)
+class MailingTryAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "customer", "message", "server_response", "owner")
+    list_filter = ("status", "owner", "owner")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "last_name", "first_name")
+    search_fields = ("email", "last_name")

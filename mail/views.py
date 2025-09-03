7091f78a4bd50dd6f67bfe91633f74f0dbec7dbb
Artemova Dina <artemova@perx.ru>
2025-09-03 10:57:18 +0300

    add logging

--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,6 @@
 import datetime
-from time import strftime
+import logging
+import os
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
@@ -8,11 +9,25 @@ from django.urls import reverse_lazy
 from django.core.cache import cache
 from django.views import View
 
-from config.settings import CACHE_ENABLED, CACHE_TIME
+from config.settings import CACHE_ENABLED, CACHE_TIME, BASE_DIR
 from django.views.generic import CreateView, UpdateView, DetailView, DeleteView, ListView
 from .models import Customer, Message, Attempt, Newsletter
 from .forms import CustomerForm, MessageForm, NewsletterForm
 from .servises import MailService
+
+date_today = datetime.datetime.today().strftime("%d-%m-%Y")
+file_name = f"{date_today}_logs.log"
+log_path = os.path.join(BASE_DIR, "logs", file_name)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename=log_path,
+    filemode="a",
+    encoding="utf-8",
+)
+logger = logging.getLogger("mail_views")
 
 
 class CustomerListView(LoginRequiredMixin, ListView):
@@ -28,19 +43,25 @@ class CustomerListView(LoginRequiredMixin, ListView):
         user = self.request.user
 
         if user.has_perm("mail.can_view_all"):
+            logger.info(f"Получение списка всех клиентов для менеджера {user}")
             if CACHE_ENABLED:
                 customers = cache.get("customers_list_all")
                 if not customers:
                     customers = super().get_queryset()
                     cache.set("customers_list_all", customers, CACHE_TIME)
+                    logger.info("Список всех клиентов записан в кеш")
+                logger.info("Список всех клиентов получен из кеша")
                 return customers
             return super().get_queryset()
 
+        logger.info(f"Получение списка клиентов пользователя {user}")
         if CACHE_ENABLED:
             customers = cache.get(f"customers_list_{user.id}")
             if not customers:
                 customers = Customer.objects.filter(owner=user)
                 cache.set(f"customers_list_{user.id}", customers, CACHE_TIME)
+                logger.info(f"Список клиентов пользователя {user} записан в кеш")
+            logger.info(f"Список клиентов пользователя {user} получен из кеша")
             return customers
         return Customer.objects.filter(owner=user)
 
@@ -58,8 +79,9 @@ class CustomerDetailView(LoginRequiredMixin, DetailView):
         customer = super().get_object()
         user = self.request.user
         if not (user == customer.owner or user.has_perm("mail.can_view_all")):
+            logger.warning(f"Пользователь {user} не имеет прав на просмотр клиента {customer}")
             raise PermissionDenied
-
+        logger.info(f"Объект клиента {customer} получен пользователем {user}")
         return customer
 
 
@@ -76,10 +98,12 @@ class CustomerCreateView(CreateView):
 
         user = self.request.user
         if not user.has_perm("mail.add_customer") or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на создание клиента")
             raise PermissionDenied
         customer = form.save()
         customer.owner = user
         customer.save()
+        logger.info(f"Пользователь {user} создал клиента {customer}")
         return super().form_valid(form)
 
 
@@ -97,8 +121,9 @@ class CustomerUpdateView(UpdateView):
         customer = super().get_object()
         user = self.request.user
         if not user == customer.owner or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на редактирование клиента {customer}")
             raise PermissionDenied
-
+        logger.info(f"Пользователь {user} отредактировал клиента {customer}")
         return customer
 
 
@@ -115,8 +140,9 @@ class CustomerDeleteView(LoginRequiredMixin, DeleteView):
         customer = super().get_object()
         user = self.request.user
         if not user == customer.owner or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на удаление клиента {customer}")
             raise PermissionDenied
-
+        logger.info(f"Пользователь {user} удалил клиента {customer}")
         return customer
 
 
@@ -133,19 +159,25 @@ class MessageListView(LoginRequiredMixin, ListView):
         user = self.request.user
 
         if user.has_perm("mail.can_view_all"):
+            logger.info(f"Получение всех писем для менеджера {user}")
             if CACHE_ENABLED:
                 messages = cache.get("messages_list_all")
                 if not messages:
                     messages = super().get_queryset()
                     cache.set("messages_list_all", messages, CACHE_TIME)
+                    logger.info(f"Список всех писем записан в кеш")
+                logger.info(f"Список всех писем получен из кеша")
                 return messages
             return super().get_queryset()
 
+        logger.info(f"Получение всех писем пользователя {user}")
         if CACHE_ENABLED:
             messages = cache.get(f"messages_list_{user.id}")
             if not messages:
                 messages = Message.objects.filter(owner=user)
                 cache.set(f"messages_list_{user.id}", messages, CACHE_TIME)
+                logger.info(f"Список всех писем пользователя {user} записан в кеш")
+            logger.info(f"Список всех писем пользователя {user} получен из кеша")
             return messages
         return Message.objects.filter(owner=user)
 
@@ -163,8 +195,9 @@ class MessageDetailView(LoginRequiredMixin, DetailView):
         message = super().get_object()
         user = self.request.user
         if not (user == message.owner or user.has_perm("mail.can_view_all")):
+            logger.warning(f"У пользователя {user} нет прав на просмотр письма {message}")
             raise PermissionDenied
-
+        logger.info(f"Получен объект письма {message} для пользователя {user}")
         return message
 
 
@@ -181,10 +214,12 @@ class MessageCreateView(CreateView):
 
         user = self.request.user
         if not user.has_perm("mail.add_message") or user.is_blocked:
+            logger.warning(f"У пользователя {user} нет прав на создание писем")
             raise PermissionDenied
         message = form.save()
         message.owner = user
         message.save()
+        logger.info(f"Пользователь {user} создал письмо {message}")
         return super().form_valid(form)
 
 
@@ -202,7 +237,9 @@ class MessageUpdateView(UpdateView):
         user = self.request.user
         message = super().get_object()
         if not user == message.owner or user.is_blocked:
+            logger.warning(f"У пользователя {user} нет прав на редактирование письма {message}")
             raise PermissionDenied
+        logger.info(f"Пользователь {user} отредактировал письмо {message}")
         return message
 
 
@@ -219,8 +256,9 @@ class MessageDeleteView(LoginRequiredMixin, DeleteView):
         message = super().get_object()
         user = self.request.user
         if not user == message.owner or user.is_blocked:
+            logger.warning(f"У пользователя {user} нет прав на удаление письма {message}")
             raise PermissionDenied
-
+        logger.info(f"Пользователь {user} удалил письмо {message}")
         return message
 
 
@@ -237,19 +275,25 @@ class NewsletterListView(LoginRequiredMixin, ListView):
         user = self.request.user
 
         if user.has_perm("mail.can_view_all"):
+            logger.info(f"Получение списка всех рассылок для менеджера {user}")
             if CACHE_ENABLED:
                 newsletters = cache.get("newsletters_list_all")
                 if not newsletters:
                     newsletters = super().get_queryset()
                     cache.set("newsletters_list_all", newsletters, CACHE_TIME)
+                    logger.info("Список всех рассылок записан в кеш")
+                logger.info("Список всех рассылок получен из кеша")
                 return newsletters
             return super().get_queryset()
 
+        logger.info(f"Получение списка рассылок пользователя {user}")
         if CACHE_ENABLED:
             newsletters = cache.get(f"newsletters_list_{user.id}")
             if not newsletters:
                 newsletters = Newsletter.objects.filter(owner=user)
                 cache.set(f"messages_list_{user.id}", newsletters, CACHE_TIME)
+                logger.info(f"Список рассылок пользователя {user} записан в кеш")
+            logger.info(f"Список рассылок пользователя {user} получен из кеша")
             return newsletters
         return Newsletter.objects.filter(owner=user)
 
@@ -266,7 +310,9 @@ class NewsletterDetailView(LoginRequiredMixin, View):
                    }
         user = self.request.user
         if not (user == newsletter.owner or user.has_perm("mail.can_view_all")):
+            logger.warning(f"Пользователь {user} не имеет прав на просмотр рассылки {newsletter}")
             raise PermissionDenied
+        logger.info(f"Получен объект рассылки {newsletter} пользователем {user}")
         return render(request, "detail_newsletter.html", context=context)
 
     def post(self, request, pk):
@@ -274,7 +320,9 @@ class NewsletterDetailView(LoginRequiredMixin, View):
         customers = newsletter.customers.all()
         user = self.request.user
         if not user.has_perm("mail.can_send_newsletters") or user.is_blocked or newsletter.status != "created":
+            logger.warning(f"Пользователь {user} не имеет прав на запуск рассылки {newsletter}")
             raise PermissionDenied
+        logger.info(f"Пользователь {user} запустил рассылку {newsletter}")
         MailService.send_email(customers, newsletter, user)
         context = {"newsletter": newsletter}
         return render(request, "newsletter_start.html", context=context)
@@ -293,10 +341,12 @@ class NewsletterCreateView(CreateView):
 
         user = self.request.user
         if not user.has_perm("mail.add_newsletter") or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на создание рассылок")
             raise PermissionDenied
         newsletter = form.save()
         newsletter.owner = user
         newsletter.save()
+        logger.info(f"Пользователь {user} создал рассылку {newsletter}")
         return super().form_valid(form)
 
 
@@ -314,7 +364,9 @@ class NewsletterUpdateView(UpdateView):
         newsletter = super().get_object()
         user = self.request.user
         if not user == newsletter.owner or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на редактирование рассылки {newsletter}")
             raise PermissionDenied
+        logger.info(f"Пользователь {user} отредактировал рассылку {newsletter}")
         return newsletter
 
 
@@ -331,8 +383,9 @@ class NewsletterDeleteView(LoginRequiredMixin, DeleteView):
         newsletter = super().get_object()
         user = self.request.user
         if not user == newsletter.owner or user.is_blocked:
+            logger.warning(f"Пользователь {user} не имеет прав на удаление рассылки {newsletter}")
             raise PermissionDenied
-
+        logger.info(f"Пользователь {user} удалил рассылку {newsletter}")
         return newsletter
 
 
@@ -349,18 +402,24 @@ class AttemptListView(LoginRequiredMixin, ListView):
         user = self.request.user
 
         if user.has_perm("mail.can_view_all"):
+            logger.info(f"Получение списка всех попыток для менеджера {user}")
             if CACHE_ENABLED:
                 attempts = cache.get("attempts_list_all")
                 if not attempts:
                     attempts = super().get_queryset()
                     cache.set("attempts_list_all", attempts, CACHE_TIME)
+                    logger.info("Список всех попыток записан в кеш")
+                logger.info("Список всех попыток получен из кеша")
                 return attempts
             return super().get_queryset()
 
+        logger.info(f"Получение списка попыток пользователя {user}")
         if CACHE_ENABLED:
             attempts = cache.get(f"attempts_list_{user.id}")
             if not attempts:
                 attempts = Attempt.objects.filter(owner=user)
                 cache.set(f"attempts_list_{user.id}", attempts, CACHE_TIME)
+                logger.info(f"Список попыток пользователя {user} записан в кеш")
+            logger.info(f"Список попыток пользователя {user} получен из кеша")
             return attempts
         return Attempt.objects.filter(owner=user)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
+import datetime
+import logging
+import os
 import random
 import string
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Group
 from django.contrib.auth.views import LoginView
+from django.core.exceptions import PermissionDenied
 from django.core.mail import send_mail
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -11,13 +15,26 @@ from django.views.generic import View, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.core.cache import cache
 
-from config.settings import DEFAULT_FROM_EMAIL, HOST_NAME, CACHE_ENABLED, CACHE_TIME
+from config.settings import DEFAULT_FROM_EMAIL, HOST_NAME, CACHE_ENABLED, CACHE_TIME, BASE_DIR
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm, CustomAuthForm, CustomUserChangeManagerForm
 from .models import CustomUser
 from mail.models import Newsletter, Customer, Attempt
 
 User = get_user_model()
+date_today = datetime.datetime.today().strftime("%d-%m-%Y")
+file_name = f"{date_today}_logs.log"
+log_path = os.path.join(BASE_DIR, "logs", file_name)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename=log_path,
+    filemode="a",
+    encoding="utf-8",
+)
+logger = logging.getLogger("users_views")
 
 
 class CustomUserCreateView(CreateView):
@@ -29,12 +46,15 @@ class CustomUserCreateView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        logger.info(f"Создан пользователь {user}")
         group = Group.objects.get(name="Base_user")
         user.groups.add(group)
+        logger.info(f"Пользователь {user} добавлен в группу доступа 'Base_user'")
         user.hash = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
         user.save()
         login(self.request, user)
         self.send_welcome_email(user.email, user.hash)
+        logger.info(f"Пользователю {user} отправлено письмо для подтверждения почты {user.email}")
         return super().form_valid(form)
 
     def send_welcome_email(self, user_email, user_hash):
@@ -56,7 +76,9 @@ class CustomUserUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         """Метод получения пользователя"""
-        return self.request.user
+        user = self.request.user
+        logger.info(f"Пользователь {user} отредактировал свой профиль")
+        return user
 
 
 class CustomUserUpdateManagerView(LoginRequiredMixin, UpdateView):
@@ -75,11 +97,14 @@ class CustomUserDetailView(LoginRequiredMixin, View):
         """Метод обработки гет запроса"""
 
         user = self.request.user
+        logger.info(f"Пользователь {user} зашел в свой профиль")
         if user.has_perm("users.can_manage"):
+            logger.info(f"Пользователь {user} получил статистику по рассылкам всех пользователей")
             newsletters_all = Newsletter.objects.count()
             newsletters_active = Newsletter.objects.filter(status="started").count()
             unique_customers = Customer.objects.values('email').distinct().count()
         else:
+            logger.info(f"Пользователь {user} получил статистику по своим рассылкам")
             newsletters_all = Newsletter.objects.filter(owner=user).count()
             newsletters_active = Newsletter.objects.filter(owner=user, status="started").count()
             unique_customers = Customer.objects.filter(owner=user).values('email').distinct().count()
@@ -104,7 +129,9 @@ class CustomUserEmailConfirm(View):
                 context = {"message": f"Добро пожаловать, {user.username}!"}
                 user.is_mail_confirmed = True
                 user.save()
+                logger.info(f"Пользователь {user} подтвердил электронную почту")
             else:
+                logger.warning("Ошибочное подтверждение электронной почты")
                 context = {"message": "К сожалению пользователь не найден, пожалуйста, перейдите по ссылке из письма"}
 
         return render(request, "mail_confirm.html", context=context)
@@ -119,10 +146,11 @@ class CustomUserLoginView(LoginView):
     def form_valid(self, form):
 
         user = form.get_user()
-        print(user)
         if user and not user.is_mail_confirmed:
             form.add_error("username", "Электронная почта не подтверждена")
+            logger.warning(f"Пользователь {user} пытается войти в систему без подтверждения электронной почты")
             return self.form_invalid(form)
+        logger.info(f"Пользователь {user} вошел в систему")
         return super().form_valid(form)
 
 
@@ -139,14 +167,17 @@ class CustomUserListView(LoginRequiredMixin, ListView):
         user = self.request.user
 
         if not user.has_perm("users.can_manage"):
-            raise PermissionError
+            logger.warning(f"Пользователь {user} не имеет прав на просмотр списка пользователей")
+            raise PermissionDenied
 
+        logger.info(f"Получение списка пользователй для менеджера {user}")
         if CACHE_ENABLED:
             users = cache.get(f"users_list")
             if not users:
-                # group = Group.objects.get(name='Base_user')
                 users = CustomUser.objects.filter(groups__name='Base_user')
                 cache.set(f"users_list", users, CACHE_TIME)
+                logger.info("Список пользователей записан в кеш")
+            logger.info("Список пользователей получен из кеша")
             return users
         return CustomUser.objects.filter(groups__name='Base_user')
 
@@ -159,9 +190,11 @@ class CustomUserDetailManagerView(LoginRequiredMixin, View):
 
         manager = self.request.user
         if not manager.has_perm("users.can_manage"):
-            raise PermissionError
+            logger.warning(f"Пользователь {manager} не имеет прав на просмотр информации о пользователях системы")
+            raise PermissionDenied
 
         user = CustomUser.objects.get(id=pk)
+        logger.info(f"Получение статистики по пользователю {user} по запросу {manager}")
         success_attempts = Attempt.objects.filter(owner=user, status="success").count()
         fail_attempts = Attempt.objects.filter(owner=user, status="fail").count()
         finished_newsletters = Newsletter.objects.filter(owner=user, status="finished").count()
